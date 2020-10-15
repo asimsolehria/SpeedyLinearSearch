da1ab43ee3613bd1de0814601828cd09660c4e18
Asim Habib <de.asimhabib@gmail.com>
2020-10-15 18:16:49 +0500

    Completed method of speedy search

--- src/speedylinearsearch/SpeedyLinearSearch.java
+++ src/speedylinearsearch/SpeedyLinearSearch.java
@@ -7,4 +7,32 @@ package speedylinearsearch;
  */
 public class SpeedyLinearSearch {
     
+    public int speedySearch(int array[], int value)
+    {
+        int start=0;
+        int end=array.length-1;
+        
+        
+        int foundIndex=-1;
+        
+        while(start!=end)
+        {
+            if(array[start]==value)
+            {
+                foundIndex=start;
+                break;
+            }
+            if(array[end]==value)
+            {
+                foundIndex=end;
+                break;
+            }
+            start++;
+            end--;
+            
+        }
+        
+        return foundIndex;
+    }
+    
 }
